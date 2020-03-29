c6b1ce5c8081b0be9cc9e79fa01a66e5324af9c1
tasneemadel97 <43787938+tasneemadel97@users.noreply.github.com>
2020-03-29 19:13:06 +0200

    solved

--- main.py
+++ main.py
@@ -22,9 +22,15 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
         self.ui.pushButton.clicked.connect(self.uploadPIC)
-        # self.ui.up.clicked.connect(self.uploadPIC)
-        # self.ui.magnitude.clicked.connect(self.FTMagnitude)
+       
         self.ui.pushButton_2.clicked.connect(self.uploadPIC2)
+
+        
+        self.ui.comboBox_3.activated.connect(self.FTMagnitude)
+        self.ui.comboBox_4.activated.connect(self.FTPhase2)
+        
+
+        
 
     def uploadPIC2(self):
         self.image2 = cv2.imread('test2.jpg',0)
@@ -39,35 +45,65 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         drawthis.rotate(270) 
 
     def FTMagnitude(self):
-        self.ui.graphicsView_7.clear()
+        self.ui.graphicsView_9.clear()
         f = np.fft.fft2(self.image)
         self.magnitude = abs(f)
         self.magnitude_spectrum=20*np.log(self.magnitude)
         x= np.asarray(self.magnitude_spectrum)
         img = pg.ImageItem(x)
-        self.ui.graphicsView_7.addItem(img)
+        self.ui.graphicsView_9.addItem(img)
+
+    def FTMagnitude2(self):
+        self.ui.graphicsView_10.clear()
+        f = np.fft.fft2(self.image2)
+        self.magnitude2 = abs(f)
+        self.magnitude2_spectrum=20*np.log(self.magnitude2)
+        x= np.asarray(self.magnitude2_spectrum)
+        img = pg.ImageItem(x)
+        self.ui.graphicsView_10.addItem(img)
+
+
+
     def FTPhase(self):
-        self.ui.graphicsView_7.clear()
+        self.ui.graphicsView_9.clear()
         fourier=np.fft.fft2(self.image)
         self.phase=np.angle(fourier)
         x= np.asarray(self.phase)
         PhaseImage1Array = pg.ImageItem(x)
-        self.ui.graphicsView_7.addItem(PhaseImage1Array)
+        self.ui.graphicsView_9.addItem(PhaseImage1Array)
+
+    def FTPhase2(self):
+        self.ui.graphicsView_10.clear()
+        fourier=np.fft.fft2(self.image2)
+        self.phase2=np.angle(fourier)
+        x= np.asarray(self.phase2)
+        PhaseImage1Array = pg.ImageItem(x)
+        self.ui.graphicsView_10.addItem(PhaseImage1Array)
+
     def FTReal(self):
-        self.ui.graphicsView_7.clear()
+        self.ui.graphicsView_9.clear()
         fourier=np.fft.fft2(self.image)
         self.real=np.real(fourier)
         x= np.asarray(self.real)
         PhaseImage1Array = pg.ImageItem(x)
-        self.ui.graphicsView_7.addItem(PhaseImage1Array)
+        self.ui.graphicsView_9.addItem(PhaseImage1Array)
+
     def FTImaginary(self):
-        self.ui.graphicsView_7.clear()
+        self.ui.graphicsView_9.clear()
         fourier=np.fft.fft2(self.image)
         self.imaginary=np.imag(fourier)
         x= np.asarray(self.imaginary)
         PhaseImage1Array = pg.ImageItem(x)
-        self.ui.graphicsView_7.addItem(PhaseImage1Array)
+        self.ui.graphicsView_9.addItem(PhaseImage1Array)
 
+def main():
 
+    app = QtWidgets.QApplication(sys.argv)
+    application = ApplicationWindow()
+    application.show()
+    app.exec_()
+    
+    
+if __name__ == '__main__':
+    main()
 
-        
